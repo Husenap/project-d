1c1bbe1e3c7cd8283557c65062b25ee12f6d3705
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2023-02-20 21:54:43 +0100

    added health (#3)

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour {
     private int mana = 100;
+    private int health = 100;
 
-    public static Action<int> manaChangedAction;
+    public static Action<int> manaChanged;
+    public static Action<int> healthChanged;
 
     public void changeMana(int offset) {
         mana += offset;
-        manaChangedAction(mana);
+        manaChanged(mana);
+    }
+
+    public void changeHealth(int offset) {
+        health += offset;
+        healthChanged(health);
     }
 }
