d9206dc8b8efaeeeecdeb6aeb728b23bd56a79b9
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2023-02-20 21:23:15 +0100

    added health

--- Assets/Scripts/UI/UIManagement.cs
+++ Assets/Scripts/UI/UIManagement.cs
@@ -5,13 +5,23 @@ public class UIManagement : MonoBehaviour {
     [SerializeField]
     private TextMeshProUGUI manaLabel;
 
+    [SerializeField]
+    private TextMeshProUGUI healthLabel;
+
     private void Start() {
-        PlayerState.manaChangedAction += updateMana;
+        PlayerState.manaChanged += updateMana;
+        PlayerState.healthChanged += updateHealth;
     }
 
     public void updateMana(int current) {
         if (manaLabel) {
             manaLabel.text = $"Mana: {current}";
+        }
+    }
+
+    public void updateHealth(int current) {
+        if (healthLabel) {
+            healthLabel.text = $"Health: {current}";
         }
     }
 }
