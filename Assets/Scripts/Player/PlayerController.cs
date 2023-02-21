602a7c8610903adf921f44f76deb9f0dddeb8b57
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2023-02-21 21:39:32 +0100

    Input System (#6)
    * added input configuration
    
    * updated settings
    
    * added input bindings using InputAction
    
    * added player input to player prefab
    
    * updated input settings to only use The Input System

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerState))]
 public class PlayerController : MonoBehaviour {
@@ -10,30 +10,31 @@ public class PlayerController : MonoBehaviour {
 
     private GameObject debugCube;
 
+    [SerializeField]
+    private InputActionAsset inputActions;
+
     void Start() {
         mainCamera = Camera.main;
         agent = GetComponent<NavMeshAgent>();
         state = GetComponent<PlayerState>();
+
+        inputActions.FindAction("Move").performed += _ => Move();
+        inputActions.FindAction("Ability1").performed += _ => state.changeMana(-1);
+        inputActions.FindAction("Ability2").performed += _ => state.changeHealth(-1);
     }
 
-    void Update() {
-        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
-            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit)) {
-                if (!debugCube) {
-                    debugCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    debugCube.transform.localScale = Vector3.one * 0.1f;
-                }
-                debugCube.transform.position = hit.point;
-                debugCube.GetComponent<Renderer>().material.color = Color.red;
-                if (agent) {
-                    agent.SetDestination(hit.point);
-                }
+    private void Move() {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit)) {
+            if (!debugCube) {
+                debugCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                debugCube.transform.localScale = Vector3.one * 0.1f;
             }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Q)) {
-            state.changeMana(-10);
+            debugCube.transform.position = hit.point;
+            debugCube.GetComponent<Renderer>().material.color = Color.red;
+            if (agent) {
+                agent.SetDestination(hit.point);
+            }
         }
     }
 }
