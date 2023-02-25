3e44994b6f832979533af4874cfe7dc520b9a673
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2023-02-25 18:07:26 +0100

    fixed so that the quit button works in editor

--- ProjectD/Assets/Scripts/UI/MainMenu.cs
+++ ProjectD/Assets/Scripts/UI/MainMenu.cs
@@ -1,13 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-    // Start is called before the first frame update
-    
-    public void StartSingleplayer (){
+public class MainMenu : MonoBehaviour {
+    public void StartSingleplayer() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
@@ -16,10 +12,10 @@ public class MainMenu : MonoBehaviour
     }
 
     public void QuitGame() {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
         Application.Quit();
+#endif
     }
-
-   
-
-   
 }
