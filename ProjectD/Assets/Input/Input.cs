ae6374e08af4851746dadd07591919fff7c98ded
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2023-03-07 22:50:56 +0100

    Updated camera movement speed

--- ProjectD/Assets/Input/Input.cs
+++ ProjectD/Assets/Input/Input.cs
@@ -120,7 +120,7 @@ namespace PD
                     ""type"": ""Value"",
                     ""id"": ""881f4a93-10cf-48c9-8cc9-676f9b195dc9"",
                     ""expectedControlType"": ""Vector2"",
-                    ""processors"": ""ScaleVector2(x=15,y=15)"",
+                    ""processors"": ""ScaleVector2(x=17,y=17)"",
                     ""interactions"": """",
                     ""initialStateCheck"": true
                 },
